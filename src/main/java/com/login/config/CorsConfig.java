4a26326d778834797f26fe7e2921f1c81b6b5ac0
guido0504 <guidoferrari24@gmail.com>
2025-12-26 19:45:45 -0300

    Se agrego la nueva url a los cors

--- src/main/java/com/login/config/CorsConfig.java
+++ src/main/java/com/login/config/CorsConfig.java
@@ -14,7 +14,8 @@ public class CorsConfig {
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:4201", "https://clubvillaelvira.infinityfreeapp.com/"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200","http://localhost:4201", "https://clubvillaelvira.infinityfreeapp.com/",
+                "http://clubback-production.up.railway.app"));
         corsConfiguration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
 
